b21da92b1f46fc2355dba42d78da92070df2f9c4
RauleSarah <raulglitzk26@gmail.com>
2019-11-06 23:23:08 -0200

    FrontEnd finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaProjetosComponent } from './lista-projetos/lista-projetos.component';
+import { DetalheProjetoComponent } from './detalhe-projeto/detalhe-projeto.component';
+import { NovoProjetoComponent } from './novo-projeto/novo-projeto.component';
+import { EditarProjetoComponent } from './editar-projeto/editar-projeto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path:'listaProjetos',
+  component:ListaProjetosComponent
+},
+{ path:'detalheProjeto/:id',
+  component:DetalheProjetoComponent
+},
+{ path:'novoProjeto',
+  component:NovoProjetoComponent
+},
+{ path:'editarProjeto',
+  component:EditarProjetoComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/lista-projetos/lista-projetos.component.ts
+++ src/app/lista-projetos/lista-projetos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'has-lista-projetos',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaProjetosComponent implements OnInit {
   projetos  = [{"nome":"Projeto 1","sala":"L08","turma":"2Q"},{"nome":"Projeto 2","sala":"L09","turma":"3Q"}];
-  constructor() { }
 
+  constructor(private router : Router) { }
   ngOnInit() {
   }
-
+  detalhes(id:number){
+    this.router.navigate(['detalheProjeto',id]);
+  }
 }
