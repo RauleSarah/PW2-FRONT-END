23e4900d2ffc7e9563b7e41e31c28abc065f2f09
RauleSarah <raulglitzk@gmail.com>
2019-11-18 21:29:38 -0300

    Projeto frontend

--- src/app/novo-projeto/novo-projeto.component.ts
+++ src/app/novo-projeto/novo-projeto.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { FormBuilder} from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { ProjetoApiService } from '../service/projeto-api.service';
+import { Projeto } from '../model/projeto';
 @Component({
   selector: 'has-novo-projeto',
   templateUrl: './novo-projeto.component.html',
   styleUrls: ['./novo-projeto.component.css']
 })
 export class NovoProjetoComponent implements OnInit {
-  
-  formProjeto : FormGroup;
 
-  constructor(public FormBuilder : FormBuilder, public service : ProjetoApiService ) { }
+  formProjeto: FormGroup;
+
+  constructor(public formBuilder: FormBuilder, public service: ProjetoApiService) { }
 
   ngOnInit() {
-    this.formProjeto = this.FormBuilder.group({
-       Turma : this.FormBuilder.control(''),
-       Sala : this.FormBuilder.control(''),
-       Orientadores : this.FormBuilder.control(''),
-       Nome : this.FormBuilder.control('')
-   })
+    this.formProjeto = this.formBuilder.group({
+      Turma: this.formBuilder.control(''),
+      Sala: this.formBuilder.control(''),
+      Orientadores: this.formBuilder.control(''),
+      Nome: this.formBuilder.control('')
+    })
   }
+  onSalvar() {
+    let projeto: Projeto = this.formProjeto.value;
+    this.service.createProjeto(projeto)
+      .subscribe(data => console.log(data),
+        error => console.log(error));
+  }
+
+
 }
